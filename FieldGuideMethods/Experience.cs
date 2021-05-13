29e01ec78a4ada045114e1b205f36669c367c010
James Tiu <jamestiulawyer@gmail.com>
2021-05-13 13:16:28 -0400

    Add project files.

---
+++ FieldGuideMethods/Experience.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldGuideMethods
+{
+    public class Experience
+    {
+        public static int CalculateHearingsPerYear(int hearingsPerMonth, int monthsWithHearings)
+        {
+            // Console.WriteLine($"HearingsPerYear: " + hearingsPerMonth * monthsWithHearings);
+            return hearingsPerMonth * monthsWithHearings;
+
+        }
+    }
+}
---
+++ FieldGuideMethods/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldGuideMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfOffices = 7;
+            int lawyersPerOffice = 2;
+
+            int yearsToCompleteLawSchool = 3;
+            int activeSchoolMonths = 8;
+            int daysOfInstructionPerMonth = 20;
+
+
+
+
+            CountNumberLawyersToStaffOffice(numberOfOffices, lawyersPerOffice);
+            Console.ReadLine();
+            Console.Clear();
+            NumberOfDaysLawSchool(yearsToCompleteLawSchool, activeSchoolMonths, daysOfInstructionPerMonth);
+            Console.ReadLine();
+        }
+
+        
+
+        public static void CalculateSawStrokesToDivideWoodIntoTwoPieces(int sawStrokesPerInch, int woodDepthInInches)
+        {
+            Console.WriteLine($"Saw cuts wood in strokes: {sawStrokesPerInch * woodDepthInInches}");
+        }
+
+        public static void CountNumberLawyersToStaffOffice (int numberOfOffices, int lawyersPerOffice)
+        {
+            Console.WriteLine($"County needs this many lawyers: {numberOfOffices * lawyersPerOffice}");
+        }
+
+        public static void NumberOfDaysLawSchool(int yearsToCompleteLawSchool, int activeSchoolMonths, int daysOfInstructionPerMonth)
+        {
+            Console.WriteLine($"I spent these many days in law school {yearsToCompleteLawSchool * activeSchoolMonths * daysOfInstructionPerMonth}");
+        }
+        
+
+    }
+}
